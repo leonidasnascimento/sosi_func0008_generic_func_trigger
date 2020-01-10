78963357cad01489a528d2320e2e72e5d6c454f1
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-01-10 10:32:57 -0300

    Addition on 'x_functions_key' key

--- func/__init__.py
+++ func/__init__.py
@@ -24,6 +24,7 @@ def main(mytimer: func.TimerRequest) -> None:
         config_obj: reader = reader(SETTINGS_FILE_PATH, 'Values')
         function_url: str = config_obj.get_value("function_url")
         stock_code_list_service_url: str = config_obj.get_value("stock_code_list_service_url")
+        x_functions_key: str = config_obj.get_value("x_functions_key")
     
         # Getting stock code list
         response = requests.request("GET", stock_code_list_service_url)
@@ -36,17 +37,18 @@ def main(mytimer: func.TimerRequest) -> None:
             logging.info(code["stock"])
             function_url_aux: str = url_query_str.format(function_url, code["stock"])
 
-            threading.Thread(target=start_function, args=(function_url_aux,)).start()
+            threading.Thread(target=start_function, args=(function_url_aux,x_functions_key,)).start()
             pass 
     except Exception as ex:
         error_log = '{} -> {}'.format(utc_timestamp, str(ex))
         logging.exception(error_log)
     pass
 
-def start_function(url: str):    
+def start_function(url: str, x_functions_key: str):    
     headers = {
         'content-type': "application/json",
         'cache-control': "no-cache",
+        'x-functions-key': x_functions_key,
         'content-length': str(len(str(json).encode('utf-8')))
     }
 
