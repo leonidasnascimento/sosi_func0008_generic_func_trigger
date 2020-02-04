c2c685da20596e7bf9039ef79b12a3eb35e02f5c
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-02-04 16:44:45 -0300

    Adjustments

--- func/__init__.py
+++ func/__init__.py
@@ -15,16 +15,19 @@ from contextlib import contextmanager
 SETTINGS_FILE_PATH = pathlib.Path(
     __file__).parent.parent.__str__() + "//local.settings.json"
 
+
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
     try:
-        logging.info("Timer job 'sosi_func0008_generic_func_trigger' has begun")
+        logging.info(
+            "Timer job 'sosi_func0008_generic_func_trigger' has begun")
 
         config_obj: reader = reader(SETTINGS_FILE_PATH, 'Values')
         function_url: str = config_obj.get_value("function_url")
-        stock_code_list_service_url: str = config_obj.get_value("stock_code_list_service_url")
+        stock_code_list_service_url: str = config_obj.get_value(
+            "stock_code_list_service_url")
         x_functions_key: str = config_obj.get_value("x_functions_key")
         thread_pool = []
 
@@ -35,11 +38,12 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info("Stocks found: {0}".format(len(stk_codes)))
 
         for code in stk_codes:
-            time.sleep(1)
+            time.sleep(0.5)
 
             logging.info("Starting '{}'".format(code["stock"]))
-            
-            t: threading.Thread = threading.Thread(target=invoke_url, args=(function_url, code["stock"], x_functions_key))
+
+            t: threading.Thread = threading.Thread(target=invoke_url, args=(
+                function_url, code["stock"], x_functions_key))
 
             thread_pool.append(t)
             t.start()
@@ -47,11 +51,12 @@ def main(mytimer: func.TimerRequest) -> None:
         for thread in thread_pool:
             if thread:
                 thread.join()
-                
+
     except Exception as ex:
         error_log = '{} -> {}'.format(utc_timestamp, str(ex))
         logging.exception(error_log)
     pass
+
 
 def invoke_url(url: str, code: str, x_functions_key: str):
     querystring = {"code": code}
