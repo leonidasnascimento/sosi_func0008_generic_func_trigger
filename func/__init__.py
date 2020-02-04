9606bce2a6428cf6b1aac008810b616757c0421c
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-02-04 16:22:37 -0300

    Ajustments

--- func/__init__.py
+++ func/__init__.py
@@ -3,12 +3,14 @@ import logging
 import azure.functions as func
 import json
 import pathlib
-import threading
 import time
 import array
 import requests
+import threading
 
 from configuration_manager.reader import reader
+from functools import partial
+from contextlib import contextmanager
 
 SETTINGS_FILE_PATH = pathlib.Path(
     __file__).parent.parent.__str__() + "//local.settings.json"
@@ -19,36 +21,41 @@ def main(mytimer: func.TimerRequest) -> None:
 
     try:
         logging.info("Timer job 'sosi_func0008_generic_func_trigger' has begun")
-        
+
         config_obj: reader = reader(SETTINGS_FILE_PATH, 'Values')
         function_url: str = config_obj.get_value("function_url")
         stock_code_list_service_url: str = config_obj.get_value("stock_code_list_service_url")
         x_functions_key: str = config_obj.get_value("x_functions_key")
-    
+        thread_pool = []
+
         # Getting stock code list
-        response = requests.request("GET", stock_code_list_service_url)
-        stk_codes = json.loads(response.text)
-        req_count: int  = 0
+        with requests.request("GET", stock_code_list_service_url) as response:
+            stk_codes = json.loads(response.text)
+
+        logging.info("Stocks found: {0}".format(len(stk_codes)))
 
         for code in stk_codes:
-            logging.info(code["stock"])
-            threading.Thread(target=invoke_url, args=(function_url, code["stock"], x_functions_key)).start()
-            
-            # req_count = req_count + 1
+            time.sleep(1)
 
-            # if req_count == 100:
-            #     time.sleep(5)
-            #     req_count = 0
-            #     pass
-            pass 
+            logging.info("Starting '{}'".format(code["stock"]))
+            
+            t: threading.Thread = threading.Thread(target=invoke_url, args=(function_url, code["stock"], x_functions_key))
+
+            thread_pool.append(t)
+            t.start()
+
+        for thread in thread_pool:
+            if thread:
+                thread.join()
+                
     except Exception as ex:
         error_log = '{} -> {}'.format(utc_timestamp, str(ex))
         logging.exception(error_log)
     pass
 
-def invoke_url(url: str, code: str, x_functions_key: str):    
+def invoke_url(url: str, code: str, x_functions_key: str):
     querystring = {"code": code}
-    
+
     headers = {
         'content-type': "application/json",
         'cache-control': "no-cache",
